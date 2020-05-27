02bb964195d1a0c2043c6625e89f3f3ee4f4f4d1
David Clark <david.e.clark@ntlworld.com>
2020-05-27 16:35:33 +0100

    end of part one

--- pages/ImageChangeOnMouseOver.js
+++ pages/ImageChangeOnMouseOver.js
@@ -1,17 +1,17 @@
 import React from "react";
-
+import ImageToggleOnMouseOver from "../src/ImageToggleOnMouseOver";
 const ImageChangeOnMouseOver = () => {
   return (
     <div>
       <ImageToggleOnMouseOver
-        primageImg="/static/speakers/bw/Speaker-187.jpg"
+        primaryImg="/static/speakers/bw/Speaker-187.jpg"
         secondaryImg="/static/speakers/Speaker-187.jpg"
         alt=""
       />
       &nbsp;&nbsp;&nbsp;
       <ImageToggleOnMouseOver
-        primageImg="/static/speakers/bw/Speaker-1124.jpg"
-        secondaryImg="/static/speakers/Speaker-187.jpg"
+        primaryImg="/static/speakers/bw/Speaker-1124.jpg"
+        secondaryImg="/static/speakers/Speaker-1124.jpg"
         alt=""
       />
     </div>
--- src/ImageToggleOnMouseOver.js
+++ src/ImageToggleOnMouseOver.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useRef } from "react";
 
-const ImageToggleOnMouseOver = ({primaryImg, secondaryImg }) => {
-    return (
-        <img 
-            src={primaryImg}
-            alt=""
-            />
-    )
-},
+const ImageToggleOnMouseOver = ({ primaryImg, secondaryImg }) => {
+  const imageRef = useRef(null);
+  return (
+    <img
+      onMouseOver={() => {
+        imageRef.current.src = secondaryImg;
+      }}
+      onMouseOut={() => {
+        imageRef.current.src = primaryImg;
+      }}
+      src="/static/speakers/bw/Speaker-8590.jpg"
+      ref={imageRef}
+      alt=""
+    />
+  );
+};
+
+export default ImageToggleOnMouseOver;
